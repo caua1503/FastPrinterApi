4813be75f56a90050bedb33309159eb8379525a0
caua1503 <cauamedinax@gmail.com>
2025-06-18 17:24:12 -0300

    Update config.py

--- app/config/config.py
+++ app/config/config.py
@@ -1,13 +1,11 @@
-import os
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-caminho = os.path.dirname(os.path.abspath(__file__))
-
 
 class Config(BaseSettings):
-    model_config = SettingsConfigDict(env_file=os.path.join(caminho, ".env"), env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     REDIS_HOST: str
     REDIS_PORT: str
     DATABASE_URL: str
+    JWT_ALGORITHM: str
