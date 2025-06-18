fb66b594f1e9cd1314acdf7fadba8b5bb85e039d
caua1503 <cauamedinax@gmail.com>
2025-06-18 17:06:35 -0300

    Update security.py

--- app/core/security.py
+++ app/core/security.py
@@ -1,18 +1,21 @@
 import random
 import string
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 import jwt
+from config import Config
 from pwdlib import PasswordHash
 
 pwd_context = PasswordHash(hashers=["argon2"]).recommended()
 
 
-def get_password_hash(password: str) -> str:
+def get_password_hash(password: str, code_hash: str) -> str:
+    password += code_hash
     return pwd_context.hash(password)
 
 
-def verify_password(password: str, hashed_password: str) -> bool:
+def verify_password(password: str, code_hash: str, hashed_password: str) -> bool:
+    password += code_hash
     return pwd_context.verify(password, hashed_password)
 
 
@@ -27,9 +30,9 @@ def generate_api_key() -> str:
     return api_key
 
 
-def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=15)) -> str:
+def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=30)) -> str:
     to_encode = data.copy()
-    expire = datetime.utcnow() + expires_delta
+    expire = datetime.now(timezone.utc) + expires_delta
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, generate_random_code(32), algorithm="HS256")
+    encoded_jwt = jwt.encode(to_encode, generate_random_code(32), algorithm=Config().JWT_ALGORITHM)
     return encoded_jwt
