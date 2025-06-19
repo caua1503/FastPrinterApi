761fb2d8603bc4759eb6a96a4be3cf1d72750939
caua1503 <cauamedinax@gmail.com>
2025-06-19 20:40:25 -0300

    Alembic

--- app/config/config.py
+++ app/config/config.py
@@ -2,7 +2,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Config(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8")
 
     REDIS_HOST: str
     REDIS_PORT: str
