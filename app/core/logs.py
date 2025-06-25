8ded02950a8018f0d74ac01528d0139d1af5470c
caua1503 <cauamedinax@gmail.com>
2025-06-25 19:14:35 -0300

    Utilizando o Celery
    -Registrar logs, cria uma fila para caso tenha erro no banco ele manter em memoria e gravar depois
    -Faz o calculo pesado de previsões  as 3 da manha todo dia (vai ser possivel alterar depois)

--- app/core/logs.py
+++ app/core/logs.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 from redis.exceptions import AuthenticationError, ConnectionError, TimeoutError
 
+from app.core.task import task_create_system_log
 from app.helpers.database_helper import get_session_logs
 from app.models.logs_model import SystemLog, UserLog
 from app.schemas.logs_schema import LogLevelSchema, SystemLogSchema, UserLogSchema
@@ -44,7 +45,7 @@ async def create_redis_log(erro: Exception, level_log: Optional[LogLevelSchema] 
             service="Redis",
             timestamp=datetime.now(),
         )
-        await create_system_log(erro_log)
+        await task_create_system_log.delay(erro_log)
 
     elif isinstance(erro, TimeoutError):
         erro_log = SystemLogSchema(
@@ -54,7 +55,7 @@ async def create_redis_log(erro: Exception, level_log: Optional[LogLevelSchema] 
             service="Redis",
             timestamp=datetime.now(),
         )
-        await create_system_log(erro_log)
+        await task_create_system_log.delay(erro_log)
 
     elif isinstance(erro, AuthenticationError):
         erro_log = SystemLogSchema(
@@ -64,7 +65,7 @@ async def create_redis_log(erro: Exception, level_log: Optional[LogLevelSchema] 
             service="Redis",
             timestamp=datetime.now(),
         )
-        await create_system_log(erro_log)
+        await task_create_system_log.delay(erro_log)
 
     else:
         erro_log = SystemLogSchema(
@@ -74,4 +75,4 @@ async def create_redis_log(erro: Exception, level_log: Optional[LogLevelSchema] 
             service="Redis",
             timestamp=datetime.now(),
         )
-        await create_system_log(erro_log)
+        await task_create_system_log.delay(erro_log)
---
+++ app/core/task.py
@@ -0,0 +1,55 @@
+import asyncio
+
+from celery import Celery
+
+from app.core import get_all_printers_maintenance_info
+from app.helpers.database_helper import get_session
+from app.schemas.filter_schema import FilterBase
+from app.schemas.logs_schema import SystemLogSchema, UserLogSchema
+from app.config import Config
+
+config = Config()  # pyright: ignore
+
+celery_app = Celery(
+    "celery_worker",
+    broker=f"{config.REDIS_URL}/0",
+    backend=f"{config.REDIS_URL}/1",
+)
+
+
+@celery_app.task
+def task_get_all_printers_maintenance_info():
+    async def __execute():
+        async for session in get_session():
+            printers_maintenance_info = await get_all_printers_maintenance_info(session, filters=FilterBase())
+            return printers_maintenance_info
+
+    return asyncio.run(__execute())
+
+
+@celery_app.task(bind=True, autoretry_for=(Exception,), retry_kwargs={"max_retries": 6, "countdown": 15})
+def task_create_system_log(log: SystemLogSchema):
+    from app.core.logs import create_system_log
+
+    try:
+
+        async def __execute():
+            await create_system_log(log)
+
+        asyncio.run(__execute())
+    except Exception as e:
+        raise e
+
+
+@celery_app.task(bind=True, autoretry_for=(Exception,), retry_kwargs={"max_retries": 6, "countdown": 15})
+def task_create_user_log(log: UserLogSchema):
+    from app.core.logs import create_user_log
+
+    try:
+
+        async def __execute():
+            await create_user_log(log)
+
+        asyncio.run(__execute())
+    except Exception as e:
+        raise e
