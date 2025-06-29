2655a38c174ab52e1992c136102bd459cb99e00d
caua1503 <cauamedinax@gmail.com>
2025-06-29 13:24:53 -0300

    Atualizado as fixtures client_redis e session_redis para usar Redis do contêiner de teste
    Modifiquei o client_redis para retornar o contêiner Redis.
    
    Atualizei o session_redis para criar cliente com IP do host (get_container_host_ip()), porta exposta (get_exposed_port(6379)) e decode_responses=True.
    
    Resolve uso incorreto do Redis local, utilizando agora o Redis do contêiner de teste.

--- test/helpers/conftest.py
+++ test/helpers/conftest.py
@@ -6,11 +6,12 @@ from testcontainers.redis import RedisContainer
 
 @pytest.fixture(scope="session")
 def client_redis():
-    with RedisContainer("redis:8.0") as redis:
-        _redis = redis.get_client()
-        yield _redis
+    with RedisContainer("redis:8.0") as redis_container:
+        yield redis_container
 
 
 @pytest_asyncio.fixture()
 async def session_redis(client_redis):
-    return redis.Redis(decode_responses=True)
+    return redis.Redis(
+        host=client_redis.get_container_host_ip(), port=client_redis.get_exposed_port(6379), decode_responses=True
+    )
