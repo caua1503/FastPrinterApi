e73c25d421beb8114af79640bbf864e532a36312
caua1503 <cauamedinax@gmail.com>
2025-06-29 13:01:37 -0300

    Update history_api.py
    Adicionando o novo filtro
    Criando docstrings para melhorar no swagger

--- app/routers/api/history_api.py
+++ app/routers/api/history_api.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.security import has_access
 from app.helpers.database_helper import get_session
-from app.schemas.filter_schema import FilterPrinter
+from app.schemas.filter_schema import FilterPrinter, FilterPrinterHistory
 from app.schemas.history_schema import (
     AlertHistorySchema,
     AlertHistorySchemaDB,
@@ -61,9 +61,12 @@ history_router = APIRouter()
 @history_router.get("/recharge", status_code=HTTPStatus.OK, response_model=ListRefillHistorySchema)
 async def api_get_history_recharge(
     session: Annotated[AsyncSession, Depends(get_session)],
-    filters: Annotated[FilterPrinter, Query()],
+    filters: Annotated[FilterPrinterHistory, Query()],
     current_user=has_access(),
 ):
+    """
+    Get all historys of recharge
+    """
     result = await get_history_recharge(session, filters)
     return result
 
@@ -72,6 +75,9 @@ async def api_get_history_recharge(
 async def api_create_history_recharge(
     history: RefillHistorySchema, session: Annotated[AsyncSession, Depends(get_session)], current_user=has_access()
 ):
+    """
+    Create a new history of recharge
+    """
     return await create_history_recharge(history, session)
 
 
@@ -82,6 +88,9 @@ async def api_update_history_recharge(
     session: Annotated[AsyncSession, Depends(get_session)],
     current_user=has_access(),
 ):
+    """
+    Update a history of recharge by id
+    """
     return await update_history_recharge(id, history, session)
 
 
@@ -89,6 +98,9 @@ async def api_update_history_recharge(
 async def api_get_history_recharge_id(
     id: int, session: Annotated[AsyncSession, Depends(get_session)], current_user=has_access()
 ):
+    """
+    Get a history of recharge by id
+    """
     return await get_history_recharge_id(id, session)
 
 
@@ -96,6 +108,9 @@ async def api_get_history_recharge_id(
 async def api_delete_history_recharge(
     id: int, session: Annotated[AsyncSession, Depends(get_session)], current_user=has_access()
 ):
+    """
+    Delete a history of recharge by id
+    """
     return await delete_history_recharge(id, session)
 
 
@@ -110,6 +125,9 @@ async def api_delete_history_recharge(
 async def api_create_history_maintenance(
     history: MaintenanceHistorySchema, session: Annotated[AsyncSession, Depends(get_session)], current_user=has_access()
 ):
+    """
+    Create a new history of maintenance
+    """
     return await create_history_maintenance(history, session)
 
 
@@ -119,6 +137,9 @@ async def api_get_history_maintenance(
     filters: Annotated[FilterPrinter, Query()],
     current_user=has_access(),
 ):
+    """
+    Get all historys of maintenance
+    """
     result = await get_history_maintenance(session, filters)
     return result
 
@@ -127,6 +148,9 @@ async def api_get_history_maintenance(
 async def api_get_history_maintenance_id(
     id: int, session: Annotated[AsyncSession, Depends(get_session)], current_user=has_access()
 ):
+    """
+    Get a history of maintenance by id
+    """
     return await get_history_maintenance_id(id, session)
 
 
@@ -137,6 +161,9 @@ async def api_update_history_maintenance(
     session: Annotated[AsyncSession, Depends(get_session)],
     current_user=has_access(),
 ):
+    """
+    Update a history of maintenance by id
+    """
     return await update_history_maintenance(id, history, session)
 
 
@@ -144,6 +171,9 @@ async def api_update_history_maintenance(
 async def api_delete_history_maintenance(
     id: int, session: Annotated[AsyncSession, Depends(get_session)], current_user=has_access()
 ):
+    """
+    Delete a history of maintenance by id
+    """
     return await delete_history_maintenance(id, session)
 
 
@@ -160,6 +190,9 @@ async def api_get_history_trash(
     filters: Annotated[FilterPrinter, Query()],
     current_user=has_access(),
 ):
+    """
+    Get all historys of trash
+    """
     result = await get_history_trash(session, filters)
     return result
 
@@ -171,6 +204,9 @@ async def api_update_history_trash(
     session: Annotated[AsyncSession, Depends(get_session)],
     current_user=has_access(),
 ):
+    """
+    Update a history of trash by id
+    """
     return await update_history_trash(id, history, session)
 
 
@@ -178,6 +214,9 @@ async def api_update_history_trash(
 async def api_delete_history_trash(
     id: int, session: Annotated[AsyncSession, Depends(get_session)], current_user=has_access()
 ):
+    """
+    Delete a history of trash by id
+    """
     return await delete_history_trash(id, session)
 
 
@@ -187,6 +226,9 @@ async def api_create_history_trash(
     session: Annotated[AsyncSession, Depends(get_session)],
     current_user=has_access(),
 ):
+    """
+    Create a new history of trash
+    """
     return await create_history_trash(history, session)
 
 
@@ -196,6 +238,9 @@ async def api_get_history_alerts(
     filters: Annotated[FilterPrinter, Query()],
     current_user=has_access(),
 ):
+    """
+    Get all historys of alerts
+    """
     result = await get_history_alerts(session, filters)
     return result
 
@@ -207,6 +252,9 @@ async def api_update_history_alert(
     session: Annotated[AsyncSession, Depends(get_session)],
     current_user=has_access(),
 ):
+    """
+    Update a history of alert by id
+    """
     return await update_history_alert(id, history, session)
 
 
@@ -214,6 +262,9 @@ async def api_update_history_alert(
 async def api_delete_history_alert(
     id: int, session: Annotated[AsyncSession, Depends(get_session)], current_user=has_access()
 ):
+    """
+    Delete a history of alert by id
+    """
     return await delete_history_alert(id, session)
 
 
@@ -230,6 +281,9 @@ async def api_get_history_status(
     filters: Annotated[FilterPrinter, Query()],
     current_user=has_access(),
 ):
+    """
+    Get all historys of status
+    """
     result = await get_history_status(session, filters)
     return result
 
@@ -238,6 +292,9 @@ async def api_get_history_status(
 async def api_get_history_status_id(
     id: int, session: Annotated[AsyncSession, Depends(get_session)], current_user=has_access()
 ):
+    """
+    Get a history of status by id
+    """
     return await get_history_status_id(id, session)
 
 
@@ -245,9 +302,15 @@ async def api_get_history_status_id(
 async def api_update_history_status(
     id: int, history: StatusHistorySchema, session: Annotated[AsyncSession, Depends(get_session)]
 ):
+    """
+    Update a history of status by id
+    """
     return await update_history_status(id, history, session)
 
 
 @history_router.delete("/status/{id}", status_code=HTTPStatus.NO_CONTENT)
 async def api_delete_history_status(id: int, session: Annotated[AsyncSession, Depends(get_session)]):
+    """
+    Delete a history of status by id
+    """
     return await delete_history_status(id, session)
