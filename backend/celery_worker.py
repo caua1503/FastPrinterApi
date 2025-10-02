7df408d1578624ac949d5e7d7b751f217d4a1ab7
caua1503 <cauamedinax@gmail.com>
2025-10-02 15:56:14 -0300

    Update celery_worker.py

--- backend/celery_worker.py
+++ backend/celery_worker.py
@@ -5,10 +5,10 @@ from app.core.celery import celery_app
 celery_app.conf.beat_schedule = {
     "get_all_printers_maintenance_info": {
         "task": "app.core.celery.tasks.task.task_get_all_printers_maintenance_info",
-        "schedule": crontab(hour="2", minute="0"),
+        "schedule": crontab(hour=2, minute=0),
     },
     "clean_refresh_token_database": {
         "task": "app.core.celery.tasks.task.task_clean_refresh_token_database",
-        "schedule": crontab(hour="3", minute="0", day_of_month="1"),
+        "schedule": crontab(hour=3, minute=0, day_of_month=1),
     },
 }
